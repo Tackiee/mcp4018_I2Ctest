ad302f1e389a98ebc5b6dacc95033a2a12fbc613
19125-mnblab <al19125@shibaura-it.ac.jp>
2023-06-30 21:52:03 +0900

    modified code(missed transform data)

--- degital_pot_test.ino
+++ degital_pot_test.ino
@@ -6,6 +6,8 @@ int value = 0;
 
 int pin0 = A0;
 
+int pinVal;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -17,12 +19,12 @@ void loop() {
   Wire.beginTransmission(0x2F); //指定したアドレスのI2Cスレーブに対して送信処理を始める．MCP4018のアドレスは，0101111(2)→2F(16)．
   Wire.write(value);
   Wire.endTransmission(); //スレーブデバイス(MCP4018)へのデータ送信を完了する．
-
+  pinVal = analogRead(pin0);
   float resist = value / 127.0 * 10.0;
-  float volt = pin0 / 1024.0 * 5.0;
-  //Serial.print("resistor data = "); 
-  //Serial.print(resist);  //抵抗値(Vss→W間)を疑似表示．キロオーム(kΩ)でシリアル出力．(最大が10kΩ，全ステップ数が128なので，左のような計算をしている．）
-  //Serial.print(" kΩ ");
+  float volt = pinVal / 1024.0 * 5.0;
+  Serial.print("resistor data = "); 
+  Serial.print(resist);  //抵抗値(Vss→W間)を疑似表示．キロオーム(kΩ)でシリアル出力．(最大が10kΩ，全ステップ数が128なので，左のような計算をしている．）
+  Serial.print(" kΩ ");
   Serial.print("Voltage = ");
   Serial.print(volt);
   Serial.println(" V");
